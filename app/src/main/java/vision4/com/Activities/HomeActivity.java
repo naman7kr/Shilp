df2b027d996c9ef4e1cee4fc60a69f6c6462a156
naman7kr <naman7kr@gmail.com>
2018-11-16 00:00:40 +0530

    names changed , profile fragment added

--- app/src/main/java/vision4/com/Activities/HomeActivity.java
+++ app/src/main/java/vision4/com/Activities/HomeActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -26,22 +27,27 @@ import vision4.com.Fragments.FeedsFragment;
 import vision4.com.Fragments.HireFragment;
 import vision4.com.Fragments.JobOffersFragment;
 import vision4.com.Fragments.NotificationFragment;
+import vision4.com.Fragments.ProfileFragment;
 import vision4.com.R;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     //variables
-    GoogleSignInAccount account;
-    FragmentTransaction ft;
+    private GoogleSignInAccount account;
+    private FragmentTransaction ft;
 
     //widgets
-    TextView nav_name;
-    NavigationView navigationView;
-    DrawerLayout drawer;
-    FrameLayout fl;
-    CircleImageView nav_img;
-    Toolbar toolbar;
+    private TextView nav_name;
+    private TextView nav_email;
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private FrameLayout fl;
+    private CircleImageView nav_img;
+    private Toolbar toolbar;
+    private LinearLayout nav_layout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,9 +67,12 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         //set default fragment
         setDefaultFragment();
 
+
+
     }
 
     private void init() {
+
         toolbar =  findViewById(R.id.toolbar);
         drawer = findViewById(R.id.drawer_layout);
         navigationView =  findViewById(R.id.nav_view);
@@ -71,6 +80,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         nav_img = header.findViewById(R.id.nav_image);
         nav_name = header.findViewById(R.id.nav_user_name);
         account = GoogleSignIn.getLastSignedInAccount(this);
+
+        nav_layout = header.findViewById(R.id.nav_header_layout);
         fl = findViewById(R.id.home_container);
     }
 
@@ -82,11 +93,22 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+
     }
     private void setDefaultFragment(){
         ft = getSupportFragmentManager().beginTransaction();
         ft.add(R.id.home_container,new FeedsFragment()).commit();
         navigationView.setCheckedItem(R.id.nav_feeds);
+
+        //go to profile
+        nav_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!(getSupportFragmentManager().findFragmentById(R.id.home_container) instanceof HireFragment)) {
+                    ft.replace(R.id.home_container, new ProfileFragment()).commit();
+                }
+            }
+        });
     }
 
 
@@ -135,6 +157,12 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 }
                 break;
             case R.id.nav_hire:
+                if(!(f instanceof HireFragment)) {
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.home_container, new HireFragment()).commit();
+                }
+                break;
+            case R.id.nav_sell_products:
                 if(!(f instanceof HireFragment)) {
                     ft = getSupportFragmentManager().beginTransaction();
                     ft.replace(R.id.home_container, new HireFragment()).commit();
